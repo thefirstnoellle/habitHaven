ba4f5d2e66b1c85606db4d210c2959aebf9865d8
thefirstnoellle <simmsnoelle@gmail.com>
2024-03-13 17:01:05 -0230

    trying to make eventlistner work - still

--- js/journal.js
+++ js/journal.js
@@ -1,9 +1,9 @@
-let textArea = document.getElementById("journal-entry");
+let textarea = document.getElementById("journal-entry");
 let saveButton = document.getElementById("save-entry");
 let journalContainer = document.getElementById=("journal-container");
 
     saveButton.addEventListener("click", function() {
-        let inputData = textArea.value;
+        let inputData = textarea.value;
         journalContainer.textContent = inputData;
         console.log("I clicked the save button");
     });
