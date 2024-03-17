54141e9d367c1b4879a36510a0b7e030a153be21
thefirstnoellle <simmsnoelle@gmail.com>
2024-03-17 11:10:13 -0230

    changing nav bar links to onclick events

--- js/journal.js
+++ js/journal.js
@@ -1,15 +1,36 @@
 
+const dateSelector = document.getElementById("date-selector");
 const journalEntry = document.getElementById("journal-entry");
 const saveBtn = document.getElementById("save-entry");
 const displayJournal = document.getElementById("journal-container");
 
-saveBtn.addEventListener('click', function() {
+window.addEventListener('load', function() {
+    const savedEntries = JSON.parse(localStorage.getItem('journalEntries')) || [];
+    savedEntries.forEach(entry => {
+        const paragraph = document.createElement('p');
+        paragraph.textContent = entry;
+        displayJournal.appendChild(paragraph);
+    });
+});
 
+saveBtn.addEventListener('click', function() {
     const inputText = journalEntry.value;
+    const selectedDate = dateSelector.value;
+
+       if (!inputText || !selectedDate) {
+        alert("Please enter both the journal entry and select a date.");
+        return;
+    }
+
     const paragraph = document.createElement('p');
-    paragraph.textContent = inputText;
+    paragraph.textContent = `Date: ${selectedDate}, Entry: ${inputText}`;
     
     displayJournal.appendChild(paragraph);
-
+    
+    const savedEntries = JSON.parse(localStorage.getItem('journalEntries'));
+    savedEntries.push(inputText);
+    localStorage.setItem('journalEntries', JSON.stringify(savedEntries));
+    
+    dateSelector.value = '';
     journalEntry.value = '';
 });
---
+++ js/main.js
@@ -0,0 +1,15 @@
+function goToHome() {
+    window.location.href = "index.html";
+}
+
+function goToAbout() {
+    window.location.href = "about.html";
+}
+
+function goToInspiration() {
+    window.location.href = "inspiration.html";
+}
+
+function goToJournal() {
+    window.location.href = "journal.html";
+}
