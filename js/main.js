08382bdfc64f1aa256283589679639998fde76e6
thefirstnoellle <simmsnoelle@gmail.com>
2024-03-22 11:45:38 -0230

    Adding selector and options to new habits

--- js/main.js
+++ js/main.js
@@ -52,16 +52,63 @@ const unit = document.getElementById("selectUnit");
 saveHabit.addEventListener("click", function() {
   const habitElement = document.createElement("div");
   const habitContainer = document.getElementById("habitContainer");
+
     habitElement.textContent = habitName.value + " - Goal: " + habitGoal.value + " " + unit.value;
-    
+// add progress image
+    const progressImg = document.createElement("img");
+    progressImg.src = "images/progress-0.png";
+// create progress select element
+    const progressSelector = document.createElement("select");
+// add options to secect element    
+    const option0 = document.createElement("option");
+    progressSelector.add(option0);
+
+    const option25 = document.createElement("option");
+    option25.text = (habitGoal.value * 0.25) + " " + unit.value;
+    progressSelector.add(option25);
+
+    const option50 = document.createElement("option");
+    option50.text = (habitGoal.value * 0.5) + " " + unit.value;
+    progressSelector.add(option50);
+
+    const option75 = document.createElement("option");
+    option75.text = (habitGoal.value * 0.75) + " " + unit.value;
+    progressSelector.add(option75);
+
+    const optionComplete = document.createElement("option");
+    optionComplete.text = (habitGoal.value * 1) + " " + unit.value;
+    progressSelector.add(optionComplete);
+
+// change progress img when user selects option
+    progressSelector.addEventListener("change", function() {
+      if (progressSelector.selectedIndex === 1) {
+        progressImg.src = "images/progress-25.png";
+      } else if (progressSelector.selectedIndex === 2) {
+        progressImg.src = "images/progress-50.png";
+      } else if (progressSelector.selectedIndex === 3) {
+        progressImg.src = "images/progress-75.png";
+      } else if (progressSelector.selectedIndex === 4) {
+        progressImg.src = "images/progress-100.png"
+      } else {
+        progressImg.src = "images/progress-0.png"
+      }
+    });
+
+// display new habit after saving
     habitContainer.appendChild(habitElement);
+    habitElement.appendChild(progressSelector);
+    habitElement.appendChild(progressImg);
+    
+// close modal after saving
     modal.style.display = "none";
     habitElement.classList.add("habitElement");
-
+    progressImg.classList.add("progress");
+// clear entered values after saving
     habitName.value = "";
     habitGoal.value = "";
     unit.value = "";
 });
+
 
 // Create functions to change with progress using radio buttons
 const radioButtons = document.querySelectorAll('input[type="radio"][name="habit"]');
