546eddc801b7b485ca5729fefa20642738d37124
thefirstnoellle <simmsnoelle@gmail.com>
2024-03-22 17:53:27 -0230

    fix js navigation

---
+++ js/pages.js
@@ -0,0 +1,20 @@
+// Clicking logo brings user to homepage
+function goToHome() {
+    window.location.href = "index.html";
+}
+// Clicking About brings user to About page
+function goToAbout() {
+    window.location.href = "about.html";
+}
+// Clicking Inspiration brings user to Inspiration page
+function goToInspiration() {
+    window.location.href = "inspiration.html";
+}
+// Clicking Journal brings user to Journal page
+function goToJournal() {
+    window.location.href = "journal.html";
+}
+// Clicking Login brings user to Login page
+function goToLogin() {
+  window.location.href = "login.html";
+}
