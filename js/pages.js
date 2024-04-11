e2f2bb665f24547f8d2437a32ac6ed6ef7085cf5
thefirstnoellle <simmsnoelle@gmail.com>
2024-04-11 18:25:59 -0230

    hamburger menu

--- js/pages.js
+++ js/pages.js
@@ -1,3 +1,19 @@
+// hamburger menu
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", function() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", ()=> {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
+
+// Navigation
+
 // Clicking logo brings user to homepage
 function goToHome() {
     window.location.href = "index.html";
